5df4701c8886a4dc38f70c3263270fb5cd8e76b2
panshangqi <1187816874@qq.com>
2017-12-26 02:20:11 +0800

    udpate blogs

---
+++ apps/blogs.py
@@ -0,0 +1,7 @@
+
+from jinja_tornado import *
+import tornado.web
+class BlogsListHandler(BaseHandler):
+    def get(self):
+        self.render_html("blogs/blogs_list.html")
+
--- routs.py
+++ routs.py
@@ -3,10 +3,12 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 from apps.components import *
 from apps.plugs import *
+from apps.blogs import *
 url = [(r"/index",IndexHandler),
         (r"/home",HomeHandler),
         (r"/panEasyUI",PanEasyUIHandler),
         (r"/plugs_scroll",PlugsScrollHandler),
+        (r"/blogs_list",BlogsListHandler),
         (r"/.*",BaseHandler)
 
       ]
