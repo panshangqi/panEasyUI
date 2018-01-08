d466c34b1ad18718b267f954e056cec655dd6a92
panshangqi <1187816874@qq.com>
2018-01-09 01:22:20 +0800

    'd'

--- apps/account.py
+++ apps/account.py
@@ -1,3 +1,4 @@
+# -*- coding=utf-8 -*-
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -5,6 +6,11 @@ sys.setdefaultencoding('utf8')
 from jinja_tornado import *
 from public import *
 import tornado.web
+import time
+import sqlite3
+class SqliteUtil:
+    def connectSQL(self):
+        conn = sqlite3.connect("database/blogs_info.db")
 
 class LoginHandler(BaseHandler):
     def get(self):
@@ -12,9 +18,24 @@ class LoginHandler(BaseHandler):
     def post(self):
         username = self.get_argument('username')
         password = self.get_argument('password')
-
-        self.set_secure_cookie('username',username,expires_days=None,expires=time.time()+6000)
-        self.redirect('/blogs_essay')
+        result={}
+        try:
+            conn = sqlite3.connect("database/blogs_info.db")
+            cursor = conn.cursor()
+            sql_param = {'username':username,'password':password}
+            cursor.execute('select user_id,create_time from user_info where username=:username and password=:password', sql_param)
+            res = cursor.fetchall()
+            conn.commit()
+            cursor.close()
+            conn.close()
+            if len(res) > 0:
+                self.set_secure_cookie('username',username,expires_days=None,expires=time.time()+6000)
+                result['status']=1
+            else:
+                result['status']=0
+        except:
+            result['status']=-1
+        self.write(result)
 
 class RegisterHandler(BaseHandler):
     def get(self):
@@ -22,7 +43,30 @@ class RegisterHandler(BaseHandler):
     def post(self):
         username = self.get_argument('username')
         password = self.get_argument('password')
+        email = self.get_argument('email')
+        result={}
+        if len(username) ==0 or len(password) == 0 or len(email) == 0:
+            result['status']=0
+        else:
+            try:
+                conn = sqlite3.connect("database/blogs_info.db")
+                cursor = conn.cursor()
+                sql_param = (getGuid(),username,password,email,time.time())
+                print sql_param
+                cursor.execute('insert into user_info(user_id,username,password,email,create_time) values(?,?,?,?,?);', sql_param)
+                conn.commit()
+                cursor.close()
+                conn.close()
+                result['status']=1
+            except:
+                result['status']=-1
+        self.write(result)
 
-        self.set_secure_cookie('username',username,expires_days=None,expires=time.time()+6000)
-        self.redirect('/blogs_essay')
-
+class SendEmailCodeHandler(BaseHandler):
+    def post(self):
+        email = self.get_argument('email')
+        res = sendEmail(email,'博客注册验证码', '管好自己的验证码哦 ：<span style="color:#ff00ff">200852</span>')
+        if res == True:
+            self.write({'status':1})
+        else:
+            self.write({'status':0})
--- apps/public.py
+++ apps/public.py
@@ -6,9 +6,32 @@ sys.setdefaultencoding('utf8')
 import md5
 import uuid
 import hashlib
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
 def getGuid():
     m_uuid = str(uuid.uuid1())
     m_md5 = hashlib.md5()
     m_md5.update(m_uuid)
     return m_md5.hexdigest()
 
+def sendEmail(toEmail,e_title,e_content):
+    msg_from='1187816874@qq.com'                                 #发送方邮箱
+    passwd='csmqytdeqvmqfjic'                                   #填入发送方邮箱的授权码
+    msg_to=toEmail                                  #收件人邮箱
+    subject=e_title                                     #主题
+    content=e_content      #正文
+    msg = MIMEText(content,'html','utf-8')
+    msg['Subject'] = Header(subject, 'utf-8').encode()
+    msg['From'] = msg_from
+    msg['To'] = msg_to
+    try:
+        s = smtplib.SMTP_SSL("smtp.qq.com",465)   #邮件服务器及端口号
+        s.login(msg_from, passwd)
+        s.sendmail(msg_from, msg_to, msg.as_string())
+        return True
+    except:
+        return False
+    finally:
+        s.quit()
